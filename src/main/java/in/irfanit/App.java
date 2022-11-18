cd1cab1e909fd23bcb9c0890c9ed9a178b3bd7c4
navedpasha <74373983+navedpasha@users.noreply.github.com>
2022-11-18 15:01:17 +0530

    Update App.java

--- src/main/java/in/irfanit/App.java
+++ src/main/java/in/irfanit/App.java
@@ -1,25 +1,11 @@
 package in.irfanit;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     int 1=10
-double j=70;
+    double j=70;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
-        
-=======
-=======
-        int j=20;
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
->>>>>>> f0545fc15f6968680e0a2ca4d713a1c48254db6f
                 System.out.println( "Good Morning...!!" );
     }
 }
