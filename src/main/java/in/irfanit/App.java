01d58c0dd4c84a35b0eca861c1915cb59dc90264
Irfan Hassan <hassan.irfan81@gmail.com>
2022-11-18 14:53:56 +0530

    modified

--- src/main/java/in/irfanit/App.java
+++ src/main/java/in/irfanit/App.java
@@ -7,10 +7,11 @@ package in.irfanit;
 public class App 
 {
     int 1=10
+int j=20;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
+
         
 =======
                 System.out.println( "Good Morning...!!" );
