c8cf00f7609468a7b5017b15e936f6455832ee15
navedpasha <74373983+navedpasha@users.noreply.github.com>
2022-11-18 14:51:52 +0530

    Update App.java

--- src/main/java/in/irfanit/App.java
+++ src/main/java/in/irfanit/App.java
@@ -10,11 +10,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
-        
-=======
                 System.out.println( "Good Morning...!!" );
-
->>>>>>> d8a143bd17a4b32e2258c73edf7b3c4a3670270f
     }
 }
